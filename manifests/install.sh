590322f4b379189a3f7a33f5c7f14378234c4072
gVien <wavetech86@gmail.com>
2016-07-15 11:15:11 -0700

    use sh instead of bash for shell files

--- manifests/install.sh
+++ manifests/install.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 cd
 
+echo "gem: --no-document" > ~/.gemrc
 # Install Ruby
 rbenv install -v 2.1.0  # it's better to put this inside the .pp manifest to ensure it's installed
 rbenv global 2.1.0
-echo "gem: --no-document" > ~/.gemrc
 # install Rails
 gem install bundler
 gem install rails -v 4.2.6   # may freeze for some reasons
